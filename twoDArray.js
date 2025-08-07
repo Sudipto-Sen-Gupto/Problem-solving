01b4b2a8459cbe8d9f0bc7ecfa530f8ec6b5b49f
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-07 08:36:23 -0500

    two dimensional array

---
+++ twoDArray.js
@@ -0,0 +1,21 @@
+// Task 7
+// Given a 2D array, update the value at second row first item to 99 and print the updated array
+
+ let number=[
+  [1, 2],
+  [3, 4],
+  [5, 6]
+];
+ 
+number[1][0]=99;
+console.log(number);
+for(let x of number){
+
+}
+// Expected Array:
+
+// [
+//   [1, 2],
+//   [99, 4],
+//   [5, 6]
+// ]
