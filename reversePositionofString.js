4b16d203f8a673a493bfd9ebd23bc97c415cfd50
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-07 07:49:30 -0500

    reversing string position

---
+++ reversePositionofString.js
@@ -0,0 +1,9 @@
+// Task 4 (Hard)
+// Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+
+let statement = 'I am a hard working person'
+// let statement1=statement.split("").reverse().join("")
+// console.log(statement1);
+
+let revPos=statement.split(" ").reverse().join(" ");
+console.log(revPos);
